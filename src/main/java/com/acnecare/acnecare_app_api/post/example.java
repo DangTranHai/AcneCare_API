ecea3a9e2a9dc4cebc38432d66c295e4438044c3
Nguyễn Văn Tú Anh <145957834+nguyenAnh132@users.noreply.github.com>
2026-04-04 10:12:50 +0700

    refactor: restructure project

--- src/main/java/com/acnecare/acnecare_app_api/post/example.java
+++ src/main/java/com/acnecare/acnecare_app_api/post/example.java
@@ -1,4 +1,4 @@
-package com.acnecare.acnecare_app_api.admin;
+package com.acnecare.acnecare_app_api.post;
 
 public class example {
 
--- src/main/java/com/acnecare/acnecare_app_api/profile/example.java
+++ src/main/java/com/acnecare/acnecare_app_api/profile/example.java
@@ -1,4 +1,4 @@
-package com.acnecare.acnecare_app_api.admin;
+package com.acnecare.acnecare_app_api.profile;
 
 public class example {
 
